8c32c554c7253f9fa867f0e75c5d15ad3d05722b
isuru ud <pereraisuruudakara@gmail.com>
2025-01-09 08:34:00 +0530

    Add config class to handle database operation

---
+++ SpaceShoot/config.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+
+namespace SpaceShoot
+{
+    class Config
+    {
+        public SqlConnection con;
+        public string str;
+
+        public Config()
+        {
+            str = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\Desktop\Works\CSharp\SpaceShoot\SpaceShoot\SpaceShoot\DataDB.mdf;Integrated Security=True";
+
+            try
+            {
+                con = new SqlConnection(str);
+                con.Open();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error: " + e.Message);
+            }
+        }
+
+    }
+}
